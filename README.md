b56ab2848ed8f1d6568c1cc7ffc7ef7cce1e4c09
badzianga <tomekoszczypko@gmail.com>
2022-05-27 14:55:38 +0200

    Added canvas background

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Mariusz Level Editor
+
+Tiled level editor for Super Mariusz Bro written in Python and Pygame.
--- libs/editor.py
+++ libs/editor.py
@@ -1,10 +1,18 @@
 from pygame.surface import Surface
 
-from .constants import BLACK
+from .constants import BACKGROUNDS, BLACK, CANVAS_SIZE
+
 
 class Editor:
     def __init__(self, screen: Surface) -> None:
         self.screen = screen
+        self.canvas = Surface(CANVAS_SIZE)
+
+        self.theme = "red"
 
     def run(self, dt: float) -> None:
         self.screen.fill(BLACK)
+
+        self.canvas.fill(BACKGROUNDS[self.theme])
+        self.screen.blit(self.canvas, (0, 0))
+
